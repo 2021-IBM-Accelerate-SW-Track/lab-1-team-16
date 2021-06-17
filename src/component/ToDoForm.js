c416da5e720c63f26f6d1a88ee147a3291094dd8
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2021-06-16 20:13:27 -0700

    Added CheckBox, Needs placing
    the checkbox functionality is working, but it isn't appearing on create

--- src/component/ToDoForm.js
+++ src/component/ToDoForm.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
 
-
 const ToDoForm = ({ addTask }) => {
 
     const [ userInput, setUserInput ] = useState('');
+    //this const is still broken
+    const [ taskDone, setCheckBox ] = useState('');
 
+  
     const handleChange = (e) => {
-        setUserInput(e.currentTarget.value)
+        setUserInput(e.currentTarget.value);
+    }
+    
+    //this method is still broken
+    const handleClick = (e) => {
+        setCheckBox(e.currentTarget.value);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        addTask(userInput);
+        addTask(userInput );
         setUserInput("");
     }
+
     return (
         <form onSubmit={handleSubmit}>
             <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+                //this makes the checkbox
+            <input type="checkbox"  id = {"defaultunchecked"} display = "none" onChange={handleClick} style={{height: '20px', width : '15px'}}/>
             <Button onClick={handleSubmit}>Create</Button>
         </form>
     );
