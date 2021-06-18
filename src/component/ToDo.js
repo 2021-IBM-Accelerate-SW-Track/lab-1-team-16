3cd37a6fba6e0ca65bc61e8095cd5e998a1ef8a7
KyleColeman123 <k.coleman123@hotmail.com>
2021-06-18 15:04:04 -0400

    Update ToDo.js
    Added delete button and functionality.

--- src/component/ToDo.js
+++ src/component/ToDo.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { Checkbox } from '@material-ui/core';
+
+
+function deleteEntry(n){
+    var divId = document.getElementById(n);
+    if (divId != null){
+        while (divId.firstChild){
+            //divId.removeChild(divId.firstChild);
+            divId.removeChild(divId.firstChild);
+        };
+    }
+}
 
 const ToDo = ({todo, handleToggle}) => {
 
@@ -6,10 +19,12 @@ const ToDo = ({todo, handleToggle}) => {
         e.preventDefault()
         handleToggle(e.currentTarget.id)
     }
-
     return (
         <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} >
+            <Checkbox></Checkbox>
             {todo.task}
+            
+            <Button id={todo.id} onClick={deleteEntry(todo.id)} variant="contained" color="secondary">Delete</Button>
         </div>
     );
 };
