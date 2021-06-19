e8bd301f71e7a03a049898728ef6b7051c843fb7
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2021-06-18 23:36:42 -0500

    Deleted Unused Code

--- src/component/ToDoForm.js
+++ src/component/ToDoForm.js
@@ -4,19 +4,11 @@ import { Button } from '@material-ui/core';
 const ToDoForm = ({ addTask }) => {
 
     const [ userInput, setUserInput ] = useState('');
-    //this const is still broken
-    const [ taskDone, setCheckBox ] = useState('');
 
-  
     const handleChange = (e) => {
         setUserInput(e.currentTarget.value);
     }
     
-    //this method is still broken
-    const handleClick = (e) => {
-        setCheckBox(e.currentTarget.value);
-    }
-
     const handleSubmit = (e) => {
         e.preventDefault();
         addTask(userInput );
@@ -26,8 +18,6 @@ const ToDoForm = ({ addTask }) => {
     return (
         <form onSubmit={handleSubmit}>
             <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
-                //this makes the checkbox
-            <input type="checkbox"  id = {"defaultunchecked"} display = "none" onChange={handleClick} style={{height: '20px', width : '15px'}}/>
             <Button onClick={handleSubmit}>Create</Button>
         </form>
     );
