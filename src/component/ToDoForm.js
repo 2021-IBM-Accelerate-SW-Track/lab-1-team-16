5624252923dc7d3934057f9d6e1a0985abbc1512
atang816 <55515112+atang816@users.noreply.github.com>
2021-06-21 20:55:13 -0700

    Update ToDoForm.js

--- src/component/ToDoForm.js
+++ src/component/ToDoForm.js
@@ -1,24 +1,32 @@
 import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
 
-
 const ToDoForm = ({ addTask }) => {
 
     const [ userInput, setUserInput ] = useState('');
 
     const handleChange = (e) => {
-        setUserInput(e.currentTarget.value)
+        setUserInput(e.currentTarget.value);
+    }
+    
+    const handleSubmit = (e) => {
+        if(userInput === "" || !userInput.trim()){
+            window.alert("Please enter a task.");
+
+        }
+        else{
+            e.preventDefault();
+            addTask(userInput );
+            setUserInput("");
+        }
+       
+        console.log(userInput)
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        addTask(userInput);
-        setUserInput("");
-    }
     return (
         <form onSubmit={handleSubmit}>
-            <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
-            <Button onClick={handleSubmit}>Create</Button>
+            <input data-testid="new-item-input" value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+            <Button data-testid="new-item-button" onClick={handleSubmit}>Create</Button>        
         </form>
     );
 };
