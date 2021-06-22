4c861716d065f010e0f899330dbbdf7e5a381b72
KyleColeman123 <k.coleman123@hotmail.com>
2021-06-21 21:06:43 -0400

    Added Update Button

--- src/component/ToDo.js
+++ src/component/ToDo.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
+import { TextField } from '@material-ui/core';
+
+document.title = "Group 16 To Do List";
+
+function updateEntry(n){
+    var newId = n*-1;
+    var divId = document.getElementById(newId);
+    if (divId != null && n > 0){
+        document.getElementById(newId).innerHTML = document.getElementById(n+.1).value;
+        
+    }  
+}
+
+
 
 const ToDo = ({todo, handleToggle}) => {
 
@@ -7,9 +23,13 @@ const ToDo = ({todo, handleToggle}) => {
         handleToggle(e.currentTarget.id)
     }
 
+    
     return (
         <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} >
-            {todo.task}
+            <span id={todo.id*-1}>{todo.task} </span>
+            <TextField id={todo.id+.1}  variant="outlined" placeholder="Enter updated task..." size="small"></TextField>
+            <span><Button onClick={updateEntry(todo.id)} variant="contained" color="primary">Update</Button></span>
+
         </div>
     );
 };
