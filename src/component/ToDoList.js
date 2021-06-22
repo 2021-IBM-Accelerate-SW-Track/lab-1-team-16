353fcac95fbed0625d11f6688f8db70127f20e80
atang816 <55515112+atang816@users.noreply.github.com>
2021-06-21 20:55:34 -0700

    Update ToDoList.js

--- src/component/ToDoList.js
+++ src/component/ToDoList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ToDo from './ToDo';
 
-const ToDoList = ({toDoList, handleToggle}) => {
+function deleteEntry(n){
+    var divId = document.getElementById(n);
+    if (divId != null){
+        while (divId.firstChild){
+            divId.removeChild(divId.firstChild);
+        };
+    }
+}
+const ToDoList = ({toDoList}) => {
     return (
         <div>
             {toDoList.map(todo => {
                 return (
-                    <ToDo todo={todo} handleToggle={handleToggle} />
+                    <ToDo todo={todo} deleteEntry={deleteEntry} />
                 )
             })}
         </div>
