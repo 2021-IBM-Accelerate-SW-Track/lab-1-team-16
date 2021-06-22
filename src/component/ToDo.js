150393f8679dd2cb774600681f59cf055f64e5be
KyleColeman123 <k.coleman123@hotmail.com>
2021-06-21 23:13:02 -0400

    Fixed Update and Delete functions to change list items.

--- src/component/ToDo.js
+++ src/component/ToDo.js
@@ -3,29 +3,42 @@ import { Button } from '@material-ui/core';
 import { Checkbox } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
 
-function updateEntry(n){
-    var newId = n*-1;
-    var divId = document.getElementById(newId);
-    if (divId != null && n > 0){
-        document.getElementById(newId).innerHTML = document.getElementById(n+.1).value;
-        
-    }  
-}
+
 
 const ToDo = ({todo, deleteEntry}) => {
 
     const handleClick = (e) => {
         e.preventDefault()
     }
+
+    function updateEntry(n){
+        
+        var newId = n*-1;
+        var divId = document.getElementById(newId);
+        if (divId != null && n > 0){
+            document.getElementById(newId).innerHTML = document.getElementById(n+.1).value;
+        todo.task = document.getElementById(n+.1).value;
+        }  
+    }
+
+    function deleteEntries(n){
+        var divId = document.getElementById(n);
+        if (divId != null){
+            while (divId.firstChild){
+                divId.removeChild(divId.firstChild);
+            };
+        }
+        todo.task += "(Deleted)";
+    }
+    
     return (
         <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} >
             <Checkbox></Checkbox>
             <span id={todo.id*-1}>{todo.task}</span> {todo.timeAdded}
             
             <TextField id={todo.id+.1}  variant="outlined" placeholder="Enter updated task..." size="small"></TextField>
-            <Button onClick={() => updateEntry(todo.id)} variant="contained" color="primary">Update</Button>
-
-            <Button id={todo.id} onClick={() => deleteEntry(todo.id)} variant="contained" color="secondary">Delete</Button>
+            <Button onClick={() => updateEntry( todo.id)} variant="contained" color="primary">Update</Button>
+            <Button id={todo.id} onClick={() => deleteEntries(todo.id)} variant="contained" color="secondary">Delete</Button>
         </div>
     );
 };
